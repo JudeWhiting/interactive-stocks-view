6828d3e7aafdc08099d91f635b9cf182226e020b
Jude <111361323+JudeWhiting@users.noreply.github.com>
2026-01-26 22:28:48 +0000

    started refactoring

--- utils/misc.py
+++ utils/misc.py
@@ -7,9 +7,6 @@ def date_x_months_ago(today: datetime, months_ago: int, to_datetime: bool=False)
     else:
         return (today - relativedelta(months=months_ago)).strftime('%m-%Y')
 
-def get_polygon_key():
+def get_api_key():
     f = open("polygon-key.txt", "r")
     return f.readline().strip()
-
-if __name__ == "__main__":
-    print(get_polygon_key())
